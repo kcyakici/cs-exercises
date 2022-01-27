ff3e0a841eb2bc4d9f0a373e93f733629b75632e
Kürşat Çağrı Yakıcı <kcyakici@hotmail.com>
2022-01-27 14:35:03 +0300

    Initialize repo

---
+++ printHelloWorld/print.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string aFriend = "Dilşat";
+Console.WriteLine(aFriend);
+
+Console.WriteLine("Hello "+ aFriend);
+
+Console.WriteLine($"Hello {aFriend}");
---
+++ strings/Program.cs
@@ -0,0 +1,18 @@
+﻿string firstFriend = "Maria";
+string secondFriend = "Sage";
+
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} characters\nThe name {secondFriend} has {secondFriend.Length} characters");
+
+string greeting = "        Hello World!          ";
+Console.WriteLine($"[{greeting}]");
+
+string trimmedGreeting = greeting.TrimStart();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.TrimEnd();
+Console.WriteLine($"[{trimmedGreeting}]");
+
+trimmedGreeting = greeting.Trim();
+Console.WriteLine($"[{trimmedGreeting}]");
